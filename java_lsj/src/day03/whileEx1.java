48a9ea5328a1d0b6d387d4c00947ab7677779bd9
sujinlee12 <153247951+sujinlee12@users.noreply.github.com>
2023-12-12 17:18:55 +0900

    자바03일차while예제1

---
+++ java_lsj/src/day03/whileEx1.java
@@ -0,0 +1,31 @@
+package day03;
+
+public class whileEx1 {
+	
+	// while문 예제
+	public static void main(String[] args) {
+		//hello world를 5번 출력하는 예제
+		//System.out.println("hello world");
+		//System.out.println("hello world");
+		//System.out.println("hello world");
+		//System.out.println("hello world");
+		//System.out.println("hello world");
+		
+		
+		
+		int i = 1; //초기화
+		int count = 3;
+		while(i <= count) {
+			System.out.println("hello world");
+			++i; //i++; i+1; i=i+1;
+			
+			
+		}
+		/*System.out.println("hello world"); //i = 1
+		System.out.println("hello world"); //i =2
+		System.out.println("hello world"); //i =3
+		System.out.println("hello world"); //i=4*/
+		
+	}
+
+}
