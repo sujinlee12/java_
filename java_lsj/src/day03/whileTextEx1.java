b8c5c68549bfb6c1d742502f01b3db80fb4f49d1
sujinlee12 <153247951+sujinlee12@users.noreply.github.com>
2023-12-12 17:17:04 +0900

    자바03일차while

---
+++ java_lsj/src/day03/whileTextEx1.java
@@ -0,0 +1,39 @@
+package day03;
+
+public class whileTextEx1 {
+
+	public static void main(String[] args) {
+		/* 1부터 5까지 출력되는 코드를 작성하세요.
+		 * 출력 예시
+		 * 1
+		 * 2
+		 * 3
+		 * 4
+		 * 5
+		 * */
+		int i = 1;
+		int count = 5;
+		while (i <= count) {
+			System.out.println(i);
+		++i;}
+		
+			/*System.out.println(1);
+			++i;
+			System.out.println(2);
+			++i;
+			System.out.println(3);
+			++i;
+			System.out.println(4);
+			++i;
+			System.out.println(5);
+			++i;
+			*/
+			
+		/* 반복횟수 : i가 1부터(초기화 1) 5까지(작거나 같다, 크거나 같다.)1씩 증가
+		 * 규칙성 : i를 콘솔에 출력
+		 * 반복문 종료 후 : 없음
+		 *  */
+			
+	}
+
+}
