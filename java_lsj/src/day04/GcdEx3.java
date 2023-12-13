a4e5410378d9f9fb402e54e9ccdfe7b6b2eee242
sujinlee12 <153247951+sujinlee12@users.noreply.github.com>
2023-12-13 17:28:27 +0900

    자바04일차_gcd

--- java_lsj/src/day04/GcdEx3.java
+++ java_lsj/src/day04/GcdEx3.java
@@ -19,22 +19,18 @@ public class GcdEx3 {
 		 * */
 		
 			int num1 = 8, num2 =12;
-			int gcd = 1;		
-			for (int i = 1; i <= num1; i++) {if (num1 % i == 0 , num2 % i == 0)
-			{gcd = i;
-			if(num1 % i == 0 && num2 % i == 0) 
+			
+			for (int i = 1; i <= num1; i++) {if (num1 % i == 0 && num2 % i == 0)
+			{	if(num1 % i == 0 && num2 % i == 0) 
 				System.out.print((i == 1 ? "" : ", ") + i);
 				
 
 				}
 			}
-			
-		
-			}
-			
-	}
 		
 		
-	}
 		scan.close();
+}		
+		
 }
+
