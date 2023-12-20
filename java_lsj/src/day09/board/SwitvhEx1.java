fbf213e49e5aecfcd8d065a7e219876488463053
sujinlee12 <153247951+sujinlee12@users.noreply.github.com>
2023-12-20 17:37:34 +0900

    자바 9일차 :

---
+++ java_lsj/src/day09/board/SwitvhEx1.java
@@ -0,0 +1,15 @@
+package day09.board;
+
+public class SwitvhEx1 {
+
+	public static void main(String[] args) {
+		// switch문 안에 지역 변수를 선언하는 경우 발생 할 수 있는 문제점
+		for(int i=0;i<5;i++) {
+			System.out.print(i+ " ");]
+			System.out.println();
+		}for (int i =0;i<5;i++);
+		
+	System.out.println();
+	}
+
+}
