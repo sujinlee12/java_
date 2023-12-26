1382230a4f1b3ab1021b812839764fbc76749972
sujinlee12 <153247951+sujinlee12@users.noreply.github.com>
2023-12-26 12:45:17 +0900

    자바 12일차 : 상속 - 접근제어자 protected 예제

---
+++ java_lsj/src/protected1/A.java
@@ -0,0 +1,9 @@
+package protected1;
+
+public class A {
+	private int num1;
+	int num2;
+	protected int num3;
+	public int num4;
+
+}
---
+++ java_lsj/src/protected1/copy/A.java
@@ -0,0 +1,9 @@
+package protected1.copy;
+
+public class A {
+	private int num1;
+	int num2;
+	protected int num3;
+	public int num4;
+
+}
