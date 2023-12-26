87e0b4890b9a672f7ddcd3a50d2541b7541b7187
sujinlee12 <153247951+sujinlee12@users.noreply.github.com>
2023-12-26 12:44:52 +0900

    자바 12일차 : 상속-접근제어자 protected 예제

---
+++ java_lsj/src/protected2/ProtectedEx1.java
@@ -0,0 +1,29 @@
+package protected2;
+
+import protected1.A;
+
+public class ProtectedEx1 {
+
+	public static void main(String[] args) {
+		/* num1 : private
+		 * num2 : default
+		 * num3 : protected
+		 * num4 : public
+		 **/
+		A a = new A();
+		//a.num1 = 1;
+		//a.num2 = 2;
+		//a.num3 = 3;
+		a.num4 = 4;	
+		B b = new B();
+		//b.num1 = 1;
+		//default인 num2는 3B와 ProtectedEx1이 같은 패키지이기 때문에 접근할 수 있지 않을까?
+		//애초에 B도 num2에 접근할 수 없음.
+		//b.num2 = 2;
+		//같은 패키지이지만 ProtectedEx1이 B클래스의 자식 클래스가 아니기 때문에
+		//b.num3 = 3;
+		b.num4 = 4;
+	}
+
+}
+
