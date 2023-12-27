4fa311b6eeac52f67da9f1aab245c30594dd6ce0
sujinlee12 <153247951+sujinlee12@users.noreply.github.com>
2023-12-27 17:29:58 +0900

    자바 13일차 : 인터페이스 - 프로그램 예제

---
+++ java_lsj/src/day13/ConsoleProgram.java
@@ -0,0 +1,66 @@
+package day13;
+
+import java.util.Scanner;
+
+public class ConsoleProgram {
+
+	public static void main(String[] args) {
+		public class ConsolProgram implements Program{
+
+			private Scanner scan = new Scanner(System.in);
+			@Override
+			public void printMenu() {
+				System.out.println("----------메뉴-------");
+				System.out.println("1. 기능 1 ");
+				System.out.println("2. 기능 2 ");
+				System.out.println("3. 기능 3 ");
+				System.out.println("4. 종료 ");
+				System.out.println("-----------------");
+				System.out.println("메뉴 선택 :");
+			}
+
+			@Override
+			public void runMenu(int menu) {
+				switch(menu) {
+				case 1:
+					System.out.println("기능 1이 실행중입니다.");
+					break;
+				case 2:
+					System.out.println("기능 2이 실행중입니다.");
+					break;
+				case 3:
+					System.out.println("기능 3이 실행중입니다.");
+					break;
+				case 4:
+					break;
+				default:
+					System.out.println("잘못된 메뉴입니다.");
+				}
+			}
+
+			@Override
+			public void printExit() {
+				System.out.println("----------------");
+				System.out.println("프로그램을 종료합니다.");
+				System.out.println("----------------");
+			}
+
+			@Override
+			public void run() {
+				System.out.println("-----------------");
+				System.out.println("프로그램을 실행합니다.");
+				System.out.println("-----------------");
+				int menu;
+				do {
+					 //메뉴 출력
+					 printMenu();
+					 //메뉴 입력
+					menu = scan.nextInt();
+					 //메뉴실행
+					 runMenu(menu);
+					}while(menu != 4);
+		
+		
+	}
+
+}
---
+++ java_lsj/src/day13/ConsoleProgramEx1.java
@@ -0,0 +1,76 @@
+package day13;
+
+public class ConsoleProgramEx1 {
+
+	public static void main(String[] args) {
+		/*ConsolProgram cp = new ConsoleProgram();
+		cp.run();
+	*/
+		//인터페이스와 구현 클래스 사이에서 형변환이 가능
+		Program p = new ConsoleProgram();
+		p.run();
+	}
+}
+	import java.util.Scanner;
+	
+	
+
+//Program 인터페이스를 구현한 구현 클래스
+	public class ConsolProgram implements Program{
+
+	private Scanner scan = new Scanner(System.in);
+	@Override
+	public void printMenu() {
+		System.out.println("----------메뉴-------");
+		System.out.println("1. 기능 1 ");
+		System.out.println("2. 기능 2 ");
+		System.out.println("3. 기능 3 ");
+		System.out.println("4. 종료 ");
+		System.out.println("-----------------");
+		System.out.println("메뉴 선택 :");
+	}
+
+	@Override
+	public void runMenu(int menu) {
+		switch(menu) {
+		case 1:
+			System.out.println("기능 1이 실행중입니다.");
+			break;
+		case 2:
+			System.out.println("기능 2이 실행중입니다.");
+			break;
+		case 3:
+			System.out.println("기능 3이 실행중입니다.");
+			break;
+		case 4:
+			break;
+		default:
+			System.out.println("잘못된 메뉴입니다.");
+		}
+	}
+
+	@Override
+	public void printExit() {
+		System.out.println("----------------");
+		System.out.println("프로그램을 종료합니다.");
+		System.out.println("----------------");
+	}
+
+	@Override
+	public void run() {
+		System.out.println("-----------------");
+		System.out.println("프로그램을 실행합니다.");
+		System.out.println("-----------------");
+		int menu;
+		do {
+			 //메뉴 출력
+			 printMenu();
+			 //메뉴 입력
+			menu = scan.nextInt();
+			 //메뉴실행
+			 runMenu(menu);
+			}while(menu != 4);
+		
+	}
+	
+}
