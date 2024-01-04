8b17d997034dc60b9eeae6f6b10f49b4566977d1
sujinlee12 <153247951+sujinlee12@users.noreply.github.com>
2024-01-04 16:20:02 +0900

    자바 18일차 : 학생 관리 예제2

---
+++ java_lsj/src/day18/student/ServerMain.java
@@ -0,0 +1,90 @@
+package day18.student;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+private static String fileName = "src/day18/student/list.txt";
+
+
+public class ServerMain {
+	private list student
+	public static void main(String[] args) {
+		int port = 5001;
+		load();
+		System.out.println("list");
+		try {
+			ServerSocket serverSocket = new ServerSocket(port);
+			while(true) {
+				Thread t = new ServerThread(serverSocket.accept());
+				t.start();
+			}
+		} catch (IOException e) {
+			System.out.println("[예외가 발생하여 서버를 종료합니다.]");
+		}
+		save();
+		
+	}
+	private static void save() {
+		try {
+			ObjectOutputStream foos = 
+					new ObjectOutputStream(new FileOutputStream(FileName));
+			foos.writeObject(List);
+			foos.flush();
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+
+	private static void load() {
+	
+}
+@RequiredArgsConstructor
+class ServerThread extends Thread{
+	@NonNull
+	private Socket socket;
+	
+	List<Student> list;//
+	
+	private ObjectInputStream ois;//클라이언트에서 읽어오때 사용
+	private ObjectOutputStream oos;//클라이언트에 보낼 때 사용
+	
+	public void run() {
+		try {
+			//클라이언트가 요청한 기능을 실행
+			ois = new ObjectInputStream(socket.getInputStream());
+			oos = new ObjectOutputStream(socket.getOutputStream());
+			//클라이언트가 요청한 기능을 입력
+			String menu = ois.readUTF();
+			//요청한 기능을 실행 
+			switch(menu) {
+			case "LOAD":
+				load();
+				break;
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	
+		}
+		try {	
+				oos.writeObject(List);
+				oos.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
