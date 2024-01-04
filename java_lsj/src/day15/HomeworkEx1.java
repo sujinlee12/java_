00a32fc9247592999d6321617c0aeba22b9d7719
sujinlee12 <153247951+sujinlee12@users.noreply.github.com>
2024-01-04 16:20:48 +0900

    20240104

--- java_lsj/src/day15/HomeworkEx1.java
+++ java_lsj/src/day15/HomeworkEx1.java
@@ -1,9 +1,10 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class HomeworkEx1 {
 
 	private static Scanner scan = new Scanner(System.in);
-
+	private final int EXIT = 4;
         public static void main(String[] args) {
             /* 영어 단어장을 관리하는 프로그램을 작성하세요.
              * 기한은 1/5까지 github에 업로드 후 강사에게 공유(단톡방)
@@ -25,12 +26,26 @@ public class HomeworkEx1 {
              * */
         	
         	//단어 추가
-        	System.out.println("추가할 단어 :");
-        	word = scan.nextLine();
-        	if(map.containsKey(word)) {
-        		System.out.println();
-        	}
-        	//단어 수정
+        	public void run() {
+        		int menu =0;
+        		printmenu();
+        		try {
+        			//메뉴선택
+        			menu = scan.nextInt();
+        			//메뉴실행
+        			runMenu(menu);
+        		}catch(InputMismatchException e) {
+        			System.out.println("잘못된 메뉴입니다.");
+        			scan.nextLine();
+        		}
+        	
+        		}
+        		}while(menu!=EXIT);
+        
+        
+        	
+			
+        	/*단어 수정
             System.out.println("수정할 단어 : ");
             word = scan.nextLine();
             if (map.containsKey(word)) {
@@ -57,16 +72,53 @@ public class HomeworkEx1 {
                		//프로그램 종료 안내 문구
                		System.out.println("프로그램 종료!");
 
-            }
-
+            }*/
+        		
+		@Override
 		private static void runMenu(int menu) {
-			// TODO Auto-generated method stub
+			switch(menu) {
+			case 1 :
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				System.out.println("프로그램 종료");
+				break;
+			default:
+				throw new InputMismatchException();
+			}
 			
 		}
 
 		private static void printmenu() {
-			// TODO Auto-generated method stub
+			/*단어 추가
+            * -단어 수정
+            * -단어 삭제
+            * -뜻 추가
+            * -뜻 수정
+            * -뜻 삭제
+            * -단어 조회*/
+			
+			System.out.println("---메뉴----");
+			System.out.println("----단어---");
+			System.out.println("1.단어 추가");
+			System.out.println("2.단어 수정");
+			System.out.println("3.단어 삭제");
+			System.out.println("----뜻----");
+			System.out.println("4.뜻 추가");
+			System.out.println("5.뜻 수정");
+			System.out.println("6.뜻 삭제");
+			System.out.println("7.단어 조회");
+			System.out.println("메뉴 선택 : ");
+			
+			//메뉴선택
+			int menu = scan.nextInt();
+			
+			//기능실행
+			
 			
 		}
 
-            p
+            
