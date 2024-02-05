3320feb6ffa58106278d068565cb1c461e183ca5
sujinlee12 <153247951+sujinlee12@users.noreply.github.com>
2024-02-05 17:54:42 +0900

    Account

---
+++ java_lsj/src/kr/kh/account/model/vo/Type.java
@@ -0,0 +1,9 @@
+package kr.kh.account.model.vo;
+
+import lombok.Data;
+
+@Data
+public class Type {
+	private String ty_name;
+	
+}
---
+++ java_lsj/src/kr/kh/account/service/AccountService.java
@@ -0,0 +1,22 @@
+package kr.kh.account.service;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import kr.kh.account.model.vo.Category;
+import kr.kh.account.model.vo.Item;
+import kr.kh.account.model.vo.Type;
+
+
+public interface AccountService {
+
+	List<Type> getTypeList();
+
+	List<Category> getCategoryList(String type);
+
+	boolean insertItem(Item item, String type);
+	
+
+
+}
