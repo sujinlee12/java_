08d9e42200a09cbe99131cbe50e6530961b2b97d
sujinlee12 <153247951+sujinlee12@users.noreply.github.com>
2024-02-20 17:38:50 +0900

    DB 시험 대비 정리

--- sql/2. 대학생/대학생_DDL.sql
+++ sql/2. 대학생/대학생_DDL.sql
@@ -1,14 +1,26 @@
-# 대학생 관리 프로그램 물리적 설계
+USE TEST;
+# 가입된 회원 전체를 조회
+SELECT 
+    *
+FROM
+    MEMBER;
+    
+# 아이디가 ID1인 회원의 정보를 조회 
+SELECT 
+    *
+FROM# 대학생 관리 프로그램 물리적 설계
 
 # DB 생성
 DROP DATABASE IF EXISTS university;
 CREATE DATABASE IF NOT EXISTS university;
 
+# DB 선택
 USE university;
 
+# student 테이블 삭제 후 추가
 DROP TABLE IF EXISTS student;
 CREATE TABLE IF NOT EXISTS student(
-	st_num CHAR (10) PRIMARY key,
+	st_num CHAR(10) PRIMARY KEY,
     st_name VARCHAR(30) NOT NULL,
     st_major VARCHAR(15) NOT NULL,
     st_grade INT NOT NULL DEFAULT 1
@@ -16,13 +28,13 @@ CREATE TABLE IF NOT EXISTS student(
 
 # professor 테이블 삭제 후 추가
 DROP TABLE IF EXISTS professor;
-CREATE TABLE IF NOT EXISTS pofessor(
+CREATE TABLE IF NOT EXISTS professor(
 	pr_num CHAR(10) PRIMARY KEY,
     pr_name VARCHAR(30) NOT NULL,
     pr_room VARCHAR(100),
     pr_major VARCHAR(15) NOT NULL
-	
 );
+
 # lecture 테이블 삭제 후 추가
 DROP TABLE IF EXISTS lecture;
 CREATE TABLE IF NOT EXISTS lecture(
@@ -34,9 +46,9 @@ CREATE TABLE IF NOT EXISTS lecture(
     le_time INT NOT NULL DEFAULT 0,
     le_pr_num CHAR(10),
     FOREIGN KEY(le_pr_num) REFERENCES professor(pr_num)
-    
 );
-#course 테이블 삭제 후 추가
+
+# course 테이블 삭제 후 추가
 DROP TABLE IF EXISTS course;
 CREATE TABLE IF NOT EXISTS course(
 	co_num INT PRIMARY KEY AUTO_INCREMENT,
@@ -46,13 +58,34 @@ CREATE TABLE IF NOT EXISTS course(
     FOREIGN KEY(co_le_num) REFERENCES lecture(le_num)
 );
 
-# contact 테이블 삭제 후 추가
+#contact 테이블 삭제 후 추가
 DROP TABLE IF EXISTS contact;
 CREATE TABLE IF NOT EXISTS contact(
 	ct_st_num CHAR(10) PRIMARY KEY,
     ct_phone VARCHAR(13) NOT NULL,
     ct_addr VARCHAR(30) NOT NULL,
-    ct_detaul VARCHAR(30) NOT NULL DEFAULT "" ,
+    ct_detail VARCHAR(30) NOT NULL DEFAULT "",
     FOREIGN KEY(ct_st_num) REFERENCES student(st_num)
-    
 );
+    MEMBER
+WHERE
+    ID = 'ID1';
+# 모든 회원의 아이디와 이메일을 조회 
+SELECT 
+    ID, EMAIL
+FROM
+    MEMBER;
+# 24년도에 가입한 회원을 조회
+SELECT 
+    *
+FROM
+    MEMBER
+WHERE
+    REG_DATE LIKE '2024-%';
+# 이메일이 네이버인 회원을 조회 
+SELECT 
+    *
+FROM
+    MEMBER
+WHERE
+    EMAIL LIKE '%_@NAVER.COM';
--- sql/2. 대학생/대학생_DDL_ERD CLOUD 이용.sql
+++ sql/2. 대학생/대학생_DDL_ERD CLOUD 이용.sql
@@ -9,7 +9,7 @@ CREATE TABLE `student` (
 	`st_num`	char(10)	NOT NULL,
 	`st_name`	varchar(30) not null,
 	`st_major`	varchar(15) not null,
-	`st_grade`	int not null default 1
+	`st_grade`	int not null default 1	
 );
 
 DROP TABLE IF EXISTS `professor`;
@@ -17,7 +17,7 @@ DROP TABLE IF EXISTS `professor`;
 CREATE TABLE `professor` (
 	`pr_num`	char(10)	NOT NULL,
 	`pr_name`	varchar(30) not null,
-	`pr_room`	varchar(100)	NULL,
+	`pr_room`	varchar(100),
 	`pr_major`	varchar(15) not null	
 );
 
@@ -26,8 +26,8 @@ DROP TABLE IF EXISTS `lecture`;
 CREATE TABLE `lecture` (
 	`le_num`	int	primary key auto_increment,
 	`le_title`	varchar(30) not null,
-	`le_room`	varchar(20)	NULL,
-	`le_schedule`	varchar(50)not null,
+	`le_room`	varchar(20),
+	`le_schedule`	varchar(50) not null,
 	`le_point`	int not null default 0,
 	`le_time`	int not null default 0,
 	`le_pr_num`	char(10)
@@ -45,9 +45,9 @@ DROP TABLE IF EXISTS `contact`;
 
 CREATE TABLE `contact` (
 	`ct_st_num`	char(10)	NOT NULL,
-	`ct_phone`	varchar(13) NOT NULL,
+	`ct_phone`	varchar(13) not null,
 	`ct_addr`	varchar(30) not null,
-	`ct_detail`	varchar(30) not null default ""	
+	`ct_detail`	varchar(30) not null default ""
 );
 
 ALTER TABLE `student` ADD CONSTRAINT `PK_STUDENT` PRIMARY KEY (
@@ -57,7 +57,6 @@ ALTER TABLE `student` ADD CONSTRAINT `PK_STUDENT` PRIMARY KEY (
 ALTER TABLE `professor` ADD CONSTRAINT `PK_PROFESSOR` PRIMARY KEY (
 	`pr_num`
 );
-
 
 
 ALTER TABLE `contact` ADD CONSTRAINT `PK_CONTACT` PRIMARY KEY (
@@ -91,4 +90,3 @@ ALTER TABLE `contact` ADD CONSTRAINT `FK_student_TO_contact_1` FOREIGN KEY (
 REFERENCES `student` (
 	`st_num`
 );
-
