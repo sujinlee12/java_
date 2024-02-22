84e14f97faa20d25e096e808f92a24e8a0514b44
sujinlee12 <153247951+sujinlee12@users.noreply.github.com>
2024-02-22 16:50:44 +0900

    서플릿 1일차 : 회원가입 로그인 기능 구현 완료

--- servlet/servlet_project1/src/main/java/kr/kh/app/controller/LoginServlet.java
+++ servlet/servlet_project1/src/main/java/kr/kh/app/controller/LoginServlet.java
@@ -1,16 +1,25 @@
 package kr.kh.app.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.mysql.cj.Session;
+
+import kr.kh.app.model.dto.LoginDTO;
+import kr.kh.app.model.vo.MemberVO;
+import kr.kh.app.service.MemberService;
+import kr.kh.app.service.MemberServiceImp;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	private MemberService memberService = new MemberServiceImp();
    
     public LoginServlet() {
     /* 화면에서 전달한 id와 pw를 콘솔창에 출력 */ 
@@ -26,9 +35,20 @@ public class LoginServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String id = request.getParameter("id");
 		String pw = request.getParameter("pw");
-		System.out.println("id :" + id);
-		System.out.println("pw : "+ pw);
-		doGet(request, response);
+		MemberVO user = memberService.login(new LoginDTO(id,pw));
+		
+		
+		if(user != null){
+			//세션에 회원정보를 저장하여 로그인 유지
+			HttpSession sesseion = request.getSession(); //request에 있는 세션을 가져옴
+			Session.setAttribute("user",user); // 세션에 user라는 이름으로 회원 정보를 저장
+			response.sendRedirect(request.getContentType()+ "/");
+			//세션에 log
+		}else {
+			doGet(request, response);
+		}
+			
+		
 	}
 
 }
