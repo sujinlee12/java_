f7af1d16b0d30905eb63e08d03dbb9cd3e4f77ba
sujinlee12 <153247951+sujinlee12@users.noreply.github.com>
2024-02-27 14:43:03 +0900

    서블릿 4일차 : 서블릿 복습 : 게시글 등록시 게시판 선택 적용 완료

--- servlet/servlet_project2/src/main/java/kr/kh/app/dao/BoardDAO.java
+++ servlet/servlet_project2/src/main/java/kr/kh/app/dao/BoardDAO.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.apache.ibatis.annotations.Param;
 
 import kr.kh.app.model.vo.BoardVO;
+import kr.kh.app.model.vo.CommunityVO;
 
 public interface BoardDAO {
 
@@ -14,6 +15,10 @@ public interface BoardDAO {
 
 	boolean insertBoard(@Param("board")BoardVO board);
 	//메퍼에는 파람에 있는 이름 쓰기.
+
+	ArrayList<CommunityVO> selectCommunityList();
+
+	
 
 	
 	
