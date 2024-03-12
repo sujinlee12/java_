f6b2962f81f36364784c42a76218fa9068ac5642
sujinlee12 <153247951+sujinlee12@users.noreply.github.com>
2024-03-12 11:49:22 +0900

    스프링 1일차 : 데이터 전송 예제

--- spring/spring/src/main/java/kr/kh/spring/controller/HomeController.java
+++ spring/spring/src/main/java/kr/kh/spring/controller/HomeController.java
@@ -1,15 +1,15 @@
 package kr.kh.spring.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import kr.kh.spring.model.dto.TestDTO;
 
 @Controller
 public class HomeController {
@@ -20,8 +20,26 @@ public class HomeController {
 	//value : url, method :전송 방식을 get 또는 post
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Model model) {
+		//model.addAttribute("화면에서 사용할 이름","보낼 데이터");
+		model.addAttribute("name","홍길동");
 		
 		return "home";
+	}
+	
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public String homePost(Model model,TestDTO testDto/*String name,int age*/) {
+		/*
+		  System.out.println("이름 : "+ name); System.out.println("나이 : " + age);
+		  model.addAttribute("화면에서 사용할 이름","보낼 데이터"); TestDTO testDto = new
+		  TestDTO(name, age);
+		 */
+		System.out.println(testDto);
+		return "home";
+	}
+	@RequestMapping(value = "/test/{num}", method = RequestMethod.GET)
+	public String test(Model model,@PathVariable("num")int num) {
+		System.out.println("경로 데이터 : " + num);
+		return "redirect:/";
 	}
 	
 }
