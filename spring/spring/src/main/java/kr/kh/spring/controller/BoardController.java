b7219d8931942a8849c88d63f12aef7c90d50d8d
sujinlee12 <153247951+sujinlee12@users.noreply.github.com>
2024-03-15 10:08:20 +0900

    게시글 pagenation 추가

--- spring/spring/src/main/java/kr/kh/spring/controller/BoardController.java
+++ spring/spring/src/main/java/kr/kh/spring/controller/BoardController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import kr.kh.spring.model.vo.BoardVO;
 import kr.kh.spring.pagination.Criteria;
+import kr.kh.spring.pagination.PageMaker;
 import kr.kh.spring.service.BoardService;
 
 @Controller
@@ -21,10 +22,13 @@ public class BoardController {
 	@GetMapping("/board/list")
 	public String boardList(Model model, Criteria cri) {
 		cri.setPerPageNum(5);
+		//게시글 가져오기
 		ArrayList<BoardVO> list = boardService.getBoardList(cri);
-		
-		
+		int totalCount = boardService.getBoardTotalCount(cri);
+		//첫번째는 내가 정해줌 
+		PageMaker pm = new PageMaker(3,cri,totalCount);
 		model.addAttribute("list",list);
+		model.addAttribute("pm",pm);
 		return "/board/list";
 
 	}
